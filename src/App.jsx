e480e23ecd37ff81fd9f5ac445c566ad009df066
Varsha <107059364+vigneshvicky2@users.noreply.github.com>
2025-04-14 12:31:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import CodeEditor from "./components/CodeEditor";
 import LanguageSelector from "./components/LanguageSelector";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    document.title = "Online Compiler By Vignesh";
+  }, []);
   return (
     <Box minH="100vh"  bg="#0f0a19" color="gray.500" pc={6} py={8} >
       {/* <LanguageSelector/> */}
