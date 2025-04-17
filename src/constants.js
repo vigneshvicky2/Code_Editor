400ea67d03bced410c3f5e2a634b34feb41e5b8f
VIGNESH K <107059364+vigneshvicky2@users.noreply.github.com>
2025-04-17 21:56:14 +0530

    Minor ui updates

--- src/constants.js
+++ src/constants.js
@@ -36,7 +36,7 @@ greet("Vignesh")
 
   java: `public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello Vignesh");
   }
 }
 `,
